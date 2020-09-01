71b3e84d359e89871841efa29dcd885293a03f39
Ryanksa <ryanhungjen.lin@mail.utoronto.ca>
2020-09-01 12:17:38 -0400

    Moved help page to its own directory. Drew and added a logo for Ouroboros in img directory

--- cmd_help.py
+++ cmd_help.py
@@ -4,4 +4,4 @@ def help(sh):
     """
     Command @help: Opens the help page of Ouroboros
     """
-    webbrowser.open("file://" + os.path.realpath("help.html"), new=2)
+    webbrowser.open("file://" + os.path.realpath("help\\help.html"), new=2)
